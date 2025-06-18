31d87f1ae5b6582a646c098a7472ede8d3ac9bae
juanparedes26 <139470808+juanparedes26@users.noreply.github.com>
2025-06-17 19:57:31 -0500

    hola

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,12 +1,15 @@
-
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from database import db  
 from models import User
 
+class UserAdmin(ModelView):
+    column_list = ('id', 'email', 'name')  # Asegúrate de incluir 'id' aquí
+
 def setup_admin(app):
     # set optional bootswatch theme
+    app.config['SECRET_KEY'] = '1005872250' 
     app.config['FLASK_ADMIN_SWATCH'] = 'sandstone'
-    admin = Admin(app, name='microblog', template_mode='bootstrap3')
+    admin = Admin(app, name=' DATABASE', template_mode='bootstrap3')
     # Add administrative views here
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(UserAdmin(User, db.session))
